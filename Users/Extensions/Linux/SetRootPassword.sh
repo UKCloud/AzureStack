09462b719fd092d5aa640ad891c1d84225fb115e
Bailey Lawson <41685901+BaileyLawson@users.noreply.github.com>
2019-02-27 11:51:35 +0000

    Update script to enable root via SSH

--- Users/Extensions/Linux/SetRootPassword.sh
+++ Users/Extensions/Linux/SetRootPassword.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "root:$1"  | chpasswd
+sudo sed -i -e "s/PermitRootLogin prohibit-password/PermitRootLogin yes/" /etc/ssh/sshd_config
+sudo service sshd restart
+echo "root:$1" | chpasswd
 
 # RUN USING:
 # sh ChangePassword.sh <Password>
