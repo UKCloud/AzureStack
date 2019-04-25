edd3dea966886f8d72c7e2ced7ebc79238bc3c87
Bailey Lawson <41685901+BaileyLawson@users.noreply.github.com>
2019-04-25 12:10:30 +0100

    Updated InstallModules script

--- Operators/PowerShell Modules/InstallModules.ps1
+++ Operators/PowerShell Modules/InstallModules.ps1
@@ -1,69 +1,70 @@
 <#
     .SYNOPSIS
-        Creates modules manifest psd1 files.
+        Installs PowerShell modules.
 
     .DESCRIPTION
-        Creates modules manifest psd1 files so that we can convert psm1 files to proper PowerShell modules.
+        Installs all PowerShell modules contained in a folder.
 
     .PARAMETER LocalRepoPath
-        Path to your Local Repository you want to convert modules folder to proper structure. Example: "C:\PowerShellModules"
+        Path to the local repository containing the modules you want to install. Example: "C:\PowerShellModules"
 
     .EXAMPLE
         InstallModules.ps1
 
     .EXAMPLE
         InstallModules.ps1 -LocalRepoPath "C:\PowerShellModules"
-    #>
 
-[CmdletBinding(SupportsShouldProcess = $True)]
+    .NOTES
+        Works in PowerShell on Windows and Linux.
+#>
+
+[CmdletBinding(SupportsShouldProcess = $true)]
 param(
     [Parameter(Mandatory = $false)]
-    [ValidateScript( {(Test-Path $_)})]
-    [String]$LocalRepoPath
-)
-    
-# Set Local Repository Path to be current path's parent directory
-if (!$LocalRepoPath) {
+    [ValidateScript({ (Test-Path $_) })]
+    [String]
     $LocalRepoPath = "..\"
-}
+)
 
 # Declare Arrays for installed and not installed modules
 $ModulesInstalled = @()
 $ModulesNotInstalled = @()
 
 # Find modules directory in local repository
-$GetModuleDirectory = Get-ChildItem -Path $LocalRepoPath | Where-Object {$_.Name -like "modules"}
+$GetModuleDirectory = Get-ChildItem -Path $LocalRepoPath | Where-Object { $_.Name -like "*modules*" }
 $GetModuleDirectory
 
 # Find each module folder in modules directory
-$GetModuleFolders = $GetModuleDirectory | Get-ChildItem -Directory -Recurse 
+$GetModuleFolders = $GetModuleDirectory | Get-ChildItem -Directory -Recurse
 $GetModuleFolders
-       
+
 foreach ($ModuleFolder in $GetModuleFolders) {
     # Declare variables
-    $PSMFile = $ModuleFolder | Get-ChildItem -Recurse -Filter "*.psm1"
+    $PsmFile = $ModuleFolder | Get-ChildItem -Recurse -Filter "*.psm1"
 
     # Check if psm1 file exist
-    if (!$PSMFile) {
+    if (-not $PsmFile) {
         Write-Host -ForegroundColor Red "Your psm1 file does not exist in folder $($ModuleFolder.FullName)"
         Write-Error -Message "Your module will not be installed correctly" -ErrorAction Continue
         $ModulesNotInstalled += $ModuleFolder.Name
-    } else {
+    }
+    else {
         # Copy module folder to PSModule Path directory so it can be imported
         $DestinationFolder = ($env:PSModulePath -split [System.IO.Path]::PathSeparator)[2]
-        Copy-Item $ModuleFolder.FullName  -Destination $DestinationFolder -Force -Recurse
+        Copy-Item -Path $ModuleFolder.FullName -Destination $DestinationFolder -Force -Recurse
 
         # Check if module can be imported
-        $CheckModule = Get-Module -ListAvailable $ModuleFolder.Name
+        $CheckModule = Get-Module -Name $ModuleFolder.Name -ListAvailable
 
         if ([String]::IsNullOrWhiteSpace($CheckModule)) {
             Write-Error -Message "Your module: $($ModuleFolder.Name) was not installed correctly" -ErrorAction Continue
             $ModulesNotInstalled += $ModuleFolder.Name
-        } else {
+        }
+        else {
             $ModulesInstalled += $CheckModule.Name
         }
     }
 }
 
 Write-Host -ForegroundColor Green "List of installed modules is: $ModulesInstalled"
-Write-Host -ForegroundColor Red "List of not installed modules is: $ModulesNotInstalled" 
+Write-Host -ForegroundColor Red "List of not installed modules is: $ModulesNotInstalled"
