94ba0a6bdafb2f3ddb7533a3d196f668a73ece42
Bailey Lawson <41685901+BaileyLawson@users.noreply.github.com>
2019-06-20 14:43:30 +0100

    Improve Get-AzsAks output (#56)
    * Add front-end IP to Get-AzsAks
    
    * Make things clearer

--- Users/PowerShell Modules/AzureStackKubernetes/AzureStackKubernetes.psm1
+++ Users/PowerShell Modules/AzureStackKubernetes/AzureStackKubernetes.psm1
@@ -430,6 +430,8 @@ function Get-AzsAks {
             $MasterVMs = Get-AzureRmVM -ResourceGroupName $VM.ResourceGroupName | Where-Object -FilterScript { $_.Name -like "*k8s*" -and $_.Name -like "*master*" }
             $CreationVM = Get-AzureRmVM -ResourceGroupName $VM.ResourceGroupName | Where-Object -FilterScript { $_.Name -like "vmd*" }
             $Networking = (Get-AzureRmPublicIpAddress -ResourceGroupName $VM.ResourceGroupName | Where-Object -FilterScript { $_.Name -like "k8s*" })
+            $FrontEndLoadBalancer = (Get-AzureRmLoadBalancer -ResourceGroupName $VM.ResourceGroupName -Name $VM.ResourceGroupName)
+            $FrontEndIp = (Get-AzureRmPublicIpAddress -ResourceGroupName $VM.ResourceGroupName | Where-Object -FilterScript { $_.Name -like "*$($FrontEndLoadBalancer.FrontendIpConfigurations.Name)*" })
             $KubernetesDeployment = Get-AzureRmResourceGroupDeployment -ResourceGroupName $VM.ResourceGroupName | Where-Object -FilterScript { $_.TemplateLink }
             $KubernetesCluster = [PSCustomObject]@{
                 "Resource group"         = $VM.ResourceGroupName
@@ -442,8 +444,9 @@ function Get-AzsAks {
                 "Slave node VM size"     = $KubernetesDeployment.Parameters.agentPoolProfileVMSize.Value
                 "Slave pool name"        = $PoolName
                 "Admin Username"         = $KubernetesDeployment.Parameters.linuxAdminUsername.Value
-                "FQDN"                   = $Networking.DnsSettings.Fqdn
-                "PublicIP"               = $Networking.IpAddress
+                "BackEndFQDN"            = $Networking.DnsSettings.Fqdn
+                "BackEndPublicIP"        = $Networking.IpAddress
+                "FrontEndPublicIp"       = $FrontEndIp.IpAddress
                 "Creation VM Name"       = $CreationVM.Name
             }
             $ArrayOfClusters += $KubernetesCluster
