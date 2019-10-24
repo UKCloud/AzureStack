2bbfce42d8268ed5fe852bdb587ec6d72d4bd782
Chris Black <cblack@ukcloud.com>
2019-10-24 23:37:02 +0100

    change tenant id

--- Demos/Meetup-25-10-2019/PowerShell5withPowerShell6.ps1
+++ Demos/Meetup-25-10-2019/PowerShell5withPowerShell6.ps1
@@ -1,9 +1,9 @@
 # Declare endpoint
 $ArmEndpoint = "https://management.frn00006.azure.ukcloud.com"
 Add-AzureRmEnvironment -Name "AzureStackUser" -ArmEndpoint $ArmEndpoint
-$TenantId = Get-AzsDomainTenantId -TenantId "contoso.onmicrosoft.com"
-# $Domain = "contoso.onmicrosoft.com"
-# (Invoke-WebRequest -Uri https://login.windows.net/$Domain/.well-known/openid-configuration -UseBasicParsing | ConvertFrom-Json).Token_Endpoint.Split('/')[3]
+#$TenantId = Get-AzsDomainTenantId -TenantId "contoso.onmicrosoft.com"
+$Domain = "contoso.onmicrosoft.com"
+$TenantId = (Invoke-WebRequest -Uri https://login.windows.net/$Domain/.well-known/openid-configuration -UseBasicParsing | ConvertFrom-Json).Token_Endpoint.Split('/')[3]
 
 # Public Azure token
 Invoke-Command -ArgumentList $TenantId -ScriptBlock { & pwsh.exe -Command "& { [Void](Connect-AzAccount -Tenant $TenantId); `$Context = Get-AzContext; `$CachedTokens = `$Context.TokenCache.ReadItems() | Where-Object -FilterScript { `$_.TenantId -eq `$Context.Tenant.Id } | Sort-Object -Property ExpiresOn -Descending; `$AzureRmAccessToken = `$CachedTokens[0].AccessToken; `$AzureRmAccessToken | Out-File -FilePath `"C:\Token.txt`" -Encoding ascii -Force }"; $global:AccessToken = Get-Content -Path "C:\Token.txt"; Remove-Item -Path "C:\Token.txt" -Force }
